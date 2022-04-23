4922d6c73e88e27bb54f82cd91aaa29c08016f33
David <67256283+davidtqw@users.noreply.github.com>
2022-04-23 17:19:56 +0800

    Update and rename cam.py to SCL-net.py

---
+++ SCL-net.py
@@ -0,0 +1,4 @@
+We will open source the SCL-net code once our paper is accepted.
+Thank you for your interest.
+We propose a lightweight person re-identification baseline, in which the basic modules of the model are composed of a new SC-unit.
+Our SCL-net has about 2.3M parameters and without any pre-training, and it can achieve 95.2%/85.9% on Rank-1 and mAP, which can rival the most state-of-the-art large-scale models.
